6d4e179fbd34370d55ba5078e223382ee01eb854
TArkadiusz <atrzaniec1@gmail.com>
2020-05-13 20:40:05 +0200

    Add project files.

---
+++ BouncingPanel/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace BouncingPanel
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlBounce = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlBounce
+            // 
+            this.pnlBounce.BackColor = System.Drawing.Color.Black;
+            this.pnlBounce.Location = new System.Drawing.Point(124, 191);
+            this.pnlBounce.Name = "pnlBounce";
+            this.pnlBounce.Size = new System.Drawing.Size(100, 100);
+            this.pnlBounce.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 150;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.pnlBounce);
+            this.Name = "frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bouncing Panel";
+            this.Load += new System.EventHandler(this.fmBounce_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBounce;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ BouncingPanel/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingPanel
+{
+    public partial class frm : Form
+    {
+        public frm()
+        {
+            InitializeComponent();
+        }
+        int maxX, maxY;
+
+        private void fmBounce_Load(object sender, EventArgs e)
+        {
+            Form frm = (Form)sender;
+            maxX = frm.Size.Width;
+            maxY = frm.Size.Height;
+        }
+
+        private Random random = new Random((int)DateTime.Now.Ticks);
+
+        private int RandomNumber(int min, int max)
+        {
+
+            return random.Next(min, max);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int moveX = RandomNumber(0, maxX-150);
+            int moveY = RandomNumber(0, maxY-150);
+            int r = RandomNumber(0, 255);
+            int g = RandomNumber(0, 255);
+            int b = RandomNumber(0, 255);
+
+            pnlBounce.Location = new Point(moveX, moveY);
+
+            pnlBounce.BackColor = new Color();
+            pnlBounce.BackColor = Color.FromArgb(r, g, b);
+        }
+    }
+}
